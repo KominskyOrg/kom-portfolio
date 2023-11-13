db5067e1f8b4b90714f8bb12fd8ff706594a77e8
jaredkominsky <kominskyjared@gmail.com>
2023-11-13 16:45:23 -0500

    changed project descriptions

--- src/content/projectData.ts
+++ src/content/projectData.ts
@@ -2,20 +2,20 @@ const projectData = [
   {
     title: "Kominsky Portfolio",
     img: "/github-mark.svg",
-    desc: "My CV/Portfolio website, powered by Astro, serves as an interactive hub...",
+    desc: "My CV/Portfolio website, powered by Astro, serves as an interactive hub, presenting my career highlights, project links, and contact information, encapsulating my professional narrative in a sleek, minimalistic, user-friendly format.",
     url: "https://github.com/KominskyOrg/kom-portfolio",
     badge: "NEW",
   },
   {
     title: "Automated Repo Creation",
     img: "/python-logo.svg",
-    desc: "Engineered a Python automation tool to expedite repository setups...",
+    desc: "Engineered a Python automation tool to expedite repository setups and Terraform configurations, significantly enhancing the maintainability of repositories.",
     url: "https://github.com/KominskyOrg/repo-creation",
   },
   {
     title: "Home Infrastructure - Infrastructure",
     img: "/EKS Cloud.svg",
-    desc: "Developed a Terraform-based infrastructure for AWS EKS clusters...",
+    desc: "Developed a Terraform-based infrastructure for AWS EKS clusters that streamlined deployment processes and improved resource management efficiency.",
     url: "https://github.com/KominskyOrg/kominsky-aws-tf",
   },
   {
