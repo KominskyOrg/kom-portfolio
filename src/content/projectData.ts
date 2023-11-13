f8f3b45113072741c90863645fd56a668bc99644
jaredkominsky <kominskyjared@gmail.com>
2023-11-13 16:37:20 -0500

    reformatted project data

---
+++ src/content/projectData.ts
@@ -0,0 +1,29 @@
+const projectData = [
+  {
+    title: "Kominsky Portfolio",
+    img: "/github-mark.svg",
+    desc: "My CV/Portfolio website, powered by Astro, serves as an interactive hub...",
+    url: "https://github.com/KominskyOrg/kom-portfolio",
+    badge: "NEW",
+  },
+  {
+    title: "Automated Repo Creation",
+    img: "/python-logo.svg",
+    desc: "Engineered a Python automation tool to expedite repository setups...",
+    url: "https://github.com/KominskyOrg/repo-creation",
+  },
+  {
+    title: "Home Infrastructure - Infrastructure",
+    img: "/EKS Cloud.svg",
+    desc: "Developed a Terraform-based infrastructure for AWS EKS clusters...",
+    url: "https://github.com/KominskyOrg/kominsky-aws-tf",
+  },
+  {
+    title: "Kominsky Terraform Modules",
+    img: "/terraform.svg",
+    desc: "The repository is a testament to my expertise in Terraform, featuring a collection of reusable modules that demonstrate my ability to architect and manage sophisticated infrastructure with modular, scalable resources, as exemplified by my home infrastructure project.",
+    url: "https://github.com/KominskyOrg/kom-tf-modules",
+  },
+];
+
+export default projectData;
