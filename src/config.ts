e06ebd33b3f645bba48807ccd7d6e8b0c112d088
jaredkominsky <83463012+jaredkominsky@users.noreply.github.com>
2023-11-13 17:14:27 -0500

    Small edits (#5)
    * changed project descriptions
    
    * added about me page but keeping it hidden for now

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astrofy | Personal Portfolio Website Template";
+export const SITE_TITLE = "Jared Kominsky's Tech Portfolio";
 export const SITE_DESCRIPTION =
-  "Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.";
+  "Explore the professional world of Jared Kominsky, a seasoned Site Reliability Engineer and automation aficionado. Dive into a portfolio showcasing innovative projects, technical expertise, and a passion for continuous learning in the realm of software development and system architecture.";
