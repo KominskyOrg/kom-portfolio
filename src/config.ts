2c159b94d834e58bf9b252ffb77d94add1916901
jaredkominsky <kominskyjared@gmail.com>
2024-11-13 21:45:54 -0600

    [feat] - Update About Me page and improve site accessibility
    - Enabled About Me link in the sidebar menu
    - Updated SITE_TITLE and SITE_DESCRIPTION in config.ts with new descriptions
    - Revised about.astro with updated content and structure
    - Replaced div elements with semantic h1, h2, and p tags in index.astro
    - Added rel=noopener noreferrer to external links for security
    - Simplified conditional rendering in index.astro

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Jared Kominsky's Tech Portfolio";
+export const SITE_TITLE = "Jared Kominsky - Site Reliability Engineer";
 export const SITE_DESCRIPTION =
-  "Explore the professional world of Jared Kominsky, a seasoned Site Reliability Engineer and automation aficionado. Dive into a portfolio showcasing innovative projects, technical expertise, and a passion for continuous learning in the realm of software development and system architecture.";
+  "Welcome to the professional portfolio of Jared Kominsky, an experienced Site Reliability Engineer specializing in AWS, Kubernetes, Docker, Terraform, and Python. Discover innovative projects, technical expertise, and a passion for optimizing systems and implementing robust solutions in software development and system architecture.";
