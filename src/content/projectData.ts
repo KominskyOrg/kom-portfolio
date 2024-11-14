2651bb6c66f7c91aaf316b5049430b3494fa68b8
jaredkominsky <kominskyjared@gmail.com>
2024-11-13 21:23:26 -0600

    [feat] - Add new projects and update CV
    - Added Python Core Package and Stack Templates to projectData.ts with NEW badges
    - Updated image for Kominsky Auth Platform project and added NEW badge
    - Removed NEW badge from Kominsky Portfolio project
    - Deleted unused auth-icon.svg image file
    - Updated cv.astro with new work experience at Groups360 and added detailed Projects section
    - Revised introduction text in index.astro

--- src/content/projectData.ts
+++ src/content/projectData.ts
@@ -1,16 +1,30 @@
 const projectData = [
   {
+    title: "Python Core Package",
+    img: "/github-mark.svg",
+    desc: "kom_python_core serves as the foundational Python library for the organization's projects, offering essential utilities and standardized configurations to ensure consistency and efficiency across applications. It includes core modules for logging, configuration management, and common functionalities, facilitating seamless integration and streamlined development within the microservice architecture.",
+    url: "https://github.com/KominskyOrg/kom_python_core",
+    badge: "NEW",
+  },
+  {
+    title: "Stack Templates",
+    img: "/github-mark.svg",
+    desc: "This project offers template repositories tailored for building microservice architecture stacks, where the placeholder 'stack' can be seamlessly replaced with specific service names like auth, identity, or inspection. Equipped with predefined configurations and deployment scripts, these templates facilitate swift and efficient deployment to staging and development environments, streamlining the setup process for scalable and modular applications.",
+    url: "https://github.com/orgs/KominskyOrg/repositories?q=template",
+    badge: "NEW",
+  },
+  {
     title: "Kominsky Auth Platform",
-    img: "/auth-icon.svg",
+    img: "/github-mark.svg",
     desc: "Developed a full-stack authentication platform featuring a React and TypeScript frontend alongside Python-based backend services. Utilized Docker for containerization, Terraform and Helm for infrastructure as code and Kubernetes deployments, and GitHub Actions to establish robust CI/CD pipelines. This project emphasizes a DevOps-centric approach, ensuring scalable, automated, and efficient deployment and maintenance processes.",
     url: "https://github.com/orgs/KominskyOrg/repositories?q=auth",
+    badge: "NEW",
   },
   {
     title: "Kominsky Portfolio",
     img: "/github-mark.svg",
     desc: "My CV/Portfolio website, powered by Astro, serves as an interactive hub, presenting my career highlights, project links, and contact information, encapsulating my professional narrative in a sleek, minimalistic, user-friendly format.",
     url: "https://github.com/KominskyOrg/kom-portfolio",
-    badge: "NEW",
   },
   {
     title: "Automated Repo Creation",
